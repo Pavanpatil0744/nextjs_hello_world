bb5a8ae1c09d27bc41796d17d70921f0f0cf63a2
Pavanpatil0744 <109224601+Pavanpatil0744@users.noreply.github.com>
2023-02-22 21:24:54 +0530

    hey  --> hello

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Hey Get started by editing{' '}
+          Hello Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
